c316872fc9a658ceb157c92603f9de58aff9e1b5
Austin Robinson <austie@austins-mbp.ad.uvu.edu>
2019-11-25 09:40:21 -0700

    updated reqs

--- mufon_chart.py
+++ mufon_chart.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 df = pd.read_csv("ENV/mufon.csv")
 
-yax = df["Date_Submitted"].unique()[::-1]
+xax = df["Date_Submitted"].unique()[::-1]
 
-xax = df["Date_Submitted"].value_counts().sort_index()
+yax = df["Date_Submitted"].value_counts().sort_index()
 
 plt.plot(xax, color="green", linestyle="solid")
 plt.title("Daily Frequency")
@@ -14,6 +14,6 @@ plt.ylabel("Number Of Sightings")
 fig, ax = plt.subplots()
 plt.title("Daily Frequency")
 plt.ylabel("Number Of Sightings")
-xax.plot(ax=ax, kind='bar')
+yax.plot(ax=ax, kind='bar')
 
 plt.show()
